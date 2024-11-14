306393342ca9569586ac7f0641daaba6d923429c
Srikarpuri <91982587+Srikarpuri@users.noreply.github.com>
2024-11-14 12:54:02 -0500

    edit profile things

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -6,7 +6,8 @@ import GoogleAuth from './pages/GoogleAuth';
 import AddEvent from './pages/AddEvent';
 import SearchPage from './pages/SearchPage';
 import ProfilePage from './pages/ProfilePage';
-import UserProfile from './pages/EditProfile';
+import EditProfile from './pages/EditProfile';
+import UserProfile from './pages/UserProfile';
 import ProfilePictureUpload from './pages/ProfilePictureUpload';
 // import ProfilePage from './pages/ProfilePage';
 
@@ -19,6 +20,7 @@ const routes = [
   <Route path="/SearchPage" element={<SearchPage />} key="SearchPage" />,
   <Route path="/ProfilePage" element={<ProfilePage />} key="ProfilePage" />,
   <Route path="/UserProfile" element={<UserProfile />} key="UserProfile" />,
+  <Route path="/EditProfile" element={<EditProfile />} key="EditProfile" />,
   <Route path="/ProfilePictureUpload" element={<ProfilePictureUpload />} key="ProfilePictureUpload" />,
   // <Route path="/ProfilePage" element={<ProfilePage />} key="ProfilePage" />,
 
